98becf704b8803d26683952618c2e3196c55720a
Denisa Soare <denisasoare1016@gmail.com>
2025-05-08 15:33:17 +0300

    Modificarea pozitiei footerului

--- medic-line/src/app/footer.jsx
+++ medic-line/src/app/footer.jsx
@@ -3,7 +3,7 @@
 export default function Footer() {
     return (
         <footer style={{
-            position: "absolute",
+            position: "relative",
             bottom: 0,
             padding: "16px",
             borderTopLeftRadius: "8px",
